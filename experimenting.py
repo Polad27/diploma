b8af1299574389d0755ddd0cabe1a78f655dcfe0
Polad Babashev <Polad_Babashev@epam.com>
2020-01-15 18:01:33 +0300

    experiments

--- experimenting.py
+++ experimenting.py
@@ -1,3 +1,5 @@
 url = f'https://meduza.io/api/w5/search?term=искусственный интеллект&page=1&per_page=100&locale=ru'
 request = requests.get(url)
 request_js = json.loads(request.content)
+request_js['has_next']
+request_js['collection']
